769dafb52119ce19eb5ee4e5289e9e804a61ac28
NikoSula <niko.sulaqvelidze@gmail.com>
2023-07-28 22:14:19 +0400

    add javascript

---
+++ apps.js
@@ -0,0 +1,25 @@
+//დავალება #1
+
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', ()=>{
+    button.remove();
+})
+
+
+
+//დავალება #2
+
+const body = document.querySelector("body");
+const img = document.createElement('img');
+img.src = "https://fastly.picsum.photos/id/405/1400/800.jpg?hmac=4CRI7OpfDWtP4EydVd4Z-1NKSGhBmpZq8OaEXVu3be8";
+
+body.appendChild(img);
+
+
+
+//დავალება #4
+
+
+
